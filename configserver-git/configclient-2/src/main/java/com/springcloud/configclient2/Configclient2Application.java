b9583bf6e6182fb2627d5f608c565914ebb6d200
waqasdilawar <waqas.dilawar@discorp.com>
2018-12-10 21:39:13 +0500

    Added client 2, which consumed config server for station2

---
+++ configserver-git/configclient-2/src/main/java/com/springcloud/configclient2/Configclient2Application.java
@@ -0,0 +1,12 @@
+package com.springcloud.configclient2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Configclient2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Configclient2Application.class, args);
+    }
+}
